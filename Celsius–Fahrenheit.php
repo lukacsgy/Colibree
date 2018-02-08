c2abb668c639b617834500c3cc923874016bfa05
György Lukács <georgelucasletters@gmail.com>
2018-02-08 14:22:06 +0100

    kesz es fut

--- Celsius–Fahrenheit.php
+++ Celsius–Fahrenheit.php
@@ -1,8 +1,13 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    $c = (float) readline("Add meg a hőmérsékletett(Celsiusban): ");
+    
+    $f = $c * 9 / 5 + 32;
+    
+    readline("Hömérséklet Fahrenheit-ben: $f °F");
+    
+    $f = (float) readline("Add meg a hőmérsékletett(Fahrenheit): ");
+    
+    $c = ($f - 32) * 5 / 9;
+    
+    readline("Hömérséklet Fahrenheit-ben: $c °C");
