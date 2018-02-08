3f02347912234d30dbf397adcfa026d903c0a49a
György Lukács <georgelucasletters@gmail.com>
2018-02-08 14:07:12 +0100

    Most mar jol is mukopdik

--- penzezes.php
+++ penzezes.php
@@ -4,6 +4,6 @@ $szazas = (int) readline("Hány db 100-Ft.-os van: ");
 $ketszaz = (int) readline("Hány db 200-Ft.-os van: ");
 $otszaz = (int) readline("Hány db 500-Ft.-os van:");
 
-$osszeg =$szazas * 100 + $ketszazas * 200 + $otszazas * 500;
+$osszeg =$szazas * 100 + $ketszaz * 200 + $otszaz * 500;
 
 echo "A Kasszabán összesen: $osszeg";
