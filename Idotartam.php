277cd1a8715fe8978d5e9af6eabd3890efe4b467
György Lukács <georgelucasletters@gmail.com>
2018-02-08 15:09:17 +0100

    javitott valtozatt

--- Idotartam.php
+++ Idotartam.php
@@ -1,11 +1,11 @@
 <?php
 
-    $o1 = (int) readline("Add meg az Kezdo Orat: ");
-    $p1 = (int) readline("Add meg az Kezdo Perct: ");
+    $o1 = (int) readline("Add meg az Kezdo Ora-t: ");
+    $p1 = (int) readline("Add meg az Kezdo Perc-et: ");
     $m1 = (int) readline("Add meg az Kezdo Masodperc: ");
     
-    $o2 = (int) readline("Add meg az Masodik Orat: ");
-    $p2 = (int) readline("Add meg az Masodik Perct: ");
+    $o2 = (int) readline("Add meg az Masodik Ora-t: ");
+    $p2 = (int) readline("Add meg az Masodik Perc-et: ");
     $m2 = (int) readline("Add meg az Masodik Masodperc: ");
     
     if ($o1 > $o2){
@@ -14,6 +14,6 @@
     else{
         $osszeg1 = $o1 * 3600 + $p1 *  60 + $m1;
         $osszeg2 = $o2 * 3600 + $p2 *  60 + $m2;
-        $kulombseg = $osszeg2 - $osszeg1;
-        echo "A két időpont közötti külömbésg(masodpercben): . $kulombseg";
+        
+        echo "A két időpont közötti különbésg(masodpercben): " . ($osszeg2 - $osszeg1);
     }
