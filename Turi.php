16bebb4a8d87f0d62ce64333b4c4c7eb82efe50f
György Lukács <georgelucasletters@gmail.com>
2018-02-09 13:26:26 +0100

    Kesz. ugy gondoltam hogy az 1 és 2 polonál felesleges szamolni hiszen fix az ertek. Visszont 3 és utána szukseges.

--- Turi.php
+++ Turi.php
@@ -1,8 +1,15 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$portion = (int) readline("Hany darab polot vasaroltal: ");
+
+echo "Osszesen fizettel ertuk: ";
+if($portion == 1){
+	echo"500 FT-ot";
+}
+elseif ($portion == 2) {
+	echo "950 Ft-ot";
+}
+else{
+	echo $portion * 400 + 150 . " Ft-ot";
+}
 
