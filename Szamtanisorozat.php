67bde8dcb2fed7acfe352831f5b6deefdac90dd6
György Lukács <georgelucasletters@gmail.com>
2018-02-09 16:54:35 +0100

    kesz

--- Szamtanisorozat.php
+++ Szamtanisorozat.php
@@ -1,14 +1,14 @@
 <?php
 
-$number1 = (float) readline("Add meg az elso valos szamot");
-$number2 = (float) readline("Add meg az masodik valos szamot");
-$number3 = (float) readline("Add meg az harmadik valos szamot");
+$number1 = (float) readline("Add meg az elso valos szamot: ");
+$number2 = (float) readline("Add meg az masodik valos szamot: ");
+$number3 = (float) readline("Add meg az harmadik valos szamot: ");
 
 if ($number1 < 0){
-	echo "Negativ szam";
+	echo "Negativ szam ";
 }
 else{
-	echo "Nem neagtív szam";
+	echo "Nem neagtív szam ";
 }
 
 for ($i = $number1; $i <= $number3; $i += $number2){
