50edfda6508ffc39ba77415d400fe3ffdc1f9791
György Lukács <georgelucasletters@gmail.com>
2018-02-09 13:01:18 +0100

    Program kesz.

--- Pozitiv,negativ,egesz.php
+++ Pozitiv,negativ,egesz.php
@@ -1,8 +1,24 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$number = (float) readline("Add meg a szamot(tizedest . ird): ");
+
+if ($number > 0){
+	echo "A szam Pozitiv, és";
+}
+elseif ($number == 0){
+	echo "A szam NULLA, tehat";
+}
+else {
+	echo "A szam Negativ, és";
+	$number *= -1;
+}
+
+$modulo = $number - floor($number);
+
+if ($modulo == 0){
+	echo " egesz szam.\n";
+}
+else {
+	echo " nem egesz szam.\n";
+}
 
