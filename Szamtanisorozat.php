44529b2cc3868b37ca1780984c0246574bfde417
György Lukács <georgelucasletters@gmail.com>
2018-02-09 16:22:00 +0100

    Egy program bekér a felhasználótól három valós számot, kiírja, hogy az első szám negatív vagy nemnegatív, majd az első számtól indulva, a második szám által meghatározott lépésközzel halad a harmadik számig, a számsorozat elemeit kiírja. Feltételezheted, hogy az első szám kisebb, mint a harmadik, és a második szám pozitív. Ezek ellenőrzésével nem kell foglalkoznod.

---
+++ Szamtanisorozat.php
@@ -0,0 +1,16 @@
+<?php
+
+$number1 = (float) readline("Add meg az elso valos szamot");
+$number2 = (float) readline("Add meg az masodik valos szamot");
+$number3 = (float) readline("Add meg az harmadik valos szamot");
+
+if ($number1 < 0){
+	echo "Negativ szam";
+}
+else{
+	echo "Nem neagtív szam";
+}
+
+for ($i = $number1; $i <= $number3; $i += $number2){
+	echo number_format($i,1). " ";
+}
