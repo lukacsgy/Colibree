171bfc7da5eb687c64c8be2ba0014f155633f0bf
György Lukács <georgelucasletters@gmail.com>
2018-02-09 17:45:58 +0100

    ma eddig jutottam

--- Szorzotablafejlecel.php
+++ Szorzotablafejlecel.php
@@ -1,8 +1,23 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$tablesize= readline("Add meg az N szamot(max 16-ig): ");
 
+echo "  |";	
+
+/*fejléc*/
+for($i = 1; $i <= $tablesize; $i++){
+	echo "  ".$i;
+}
+echo "\n";
+
+/*vezér csik*/
+echo "--+";
+for($i = 1; $i <= $tablesize; $i++){
+	echo "---";
+}
+
+/*szorzo tabla*/
+
+for($j = 1; $j < $tablesize; $j++ ){
+	
+}
