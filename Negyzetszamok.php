af9dea4fa78cd39a8956e603068e2aa4b3962ffd
György Lukács <georgelucasletters@gmail.com>
2018-02-09 14:03:48 +0100

    Kesz. Visszont whilben ahogy kiirom az kÃerdesem lehetne e szebb?

--- Negyzetszamok.php
+++ Negyzetszamok.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$number =(int) readline("Add meg hany darab negyzetszamot adjak meg(1-től kezdve):");
+
+
+for( $i = 1; $i <= $number; $i++){
+	echo pow($i,2). "\n";
+}
+echo"\n ".$number." kisseb negyszamok pedig: ";
+
+$power = 1;
+while (pow($power,2) < $number){
+	echo $power * $power. ", ";
+	$power++;
+}	
 
