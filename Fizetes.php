f892e0981e2ef37cce59d062c9f0171cd5389cfc
György Lukács <georgelucasletters@gmail.com>
2018-02-09 13:12:26 +0100

    Kesz. Visszont logikaliag nekem az volt az otletem hogy kiseebel kezdem mert egy bizonyos ertek fölött mindig lehet magas(tehát vazt nem akkartam 2 tek koze szoritani).es arra akkor logikusabb az else nekem .

--- Fizetes.php
+++ Fizetes.php
@@ -1,8 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$salery = (int) readline("Add meg a fizetsed Ft-ban: ");
+
+if ($salery < 200000){
+	echo "Fizetesed alacsony";
+}
+elseif ($salery <= 400000) {
+	echo "Fizetesed atlagos";
+}
+else{
+	echo "Fizetesed magas!Gratulalok";
+}
 
