f5bf96a9335b841927286fb3995c8148f26528fa
György Lukács <georgelucasletters@gmail.com>
2018-02-09 15:03:36 +0100

    Kesz visszont szerintem igy nem a legszebb az elrendezés.

--- Gombok.php
+++ Gombok.php
@@ -1,8 +1,13 @@
 <?php
+$bulk = 0;
+$i = 10;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+while ($bulk < 1000000){
+	$bulk = (4 * pow($i, 3) * M_PI )/ 3;
+	if ($bulk > 1000000){		
+	}
+	else{
+		echo number_format($bulk, 2)."cm3\n";
+	}
+	$i += 10;
+}
