6267bdb690d3e44212d8a81b0c048ac5139a38f7
György Lukács <georgelucasletters@gmail.com>
2018-02-12 16:01:30 +0100

    Kesz van

--- Karaktertipusa.php
+++ Karaktertipusa.php
@@ -1,3 +1,16 @@
 <?php
 
+$charter = readline("Addj meg egy karaktert: ");
 
+if((ord($charter) >= 48) && (ord($charter) <=57)){
+	echo "Ez egy szám!";
+}
+elseif ((ord($charter) >= 65) && (ord($charter) <= 91)) {
+	echo "Ez egy nagy betű!";
+}
+elseif ((ord($charter) >= 97) && (ord($charter) <= 123)) {
+	echo "Ez egy kis betű!";
+}
+else{
+	echo "Ez egyébb karakter!";
+}
