9662570e006ebf75ec08bb67514ea35cefd719fe
György Lukács <georgelucasletters@gmail.com>
2018-02-12 11:37:31 +0100

    Kesz van.

--- Hazteto.php
+++ Hazteto.php
@@ -1,4 +1,17 @@
 <?php
 
+$number = readline("Add meg a haztető méretét: ");
 
+for ($i = 1; $i <= $number; $i++){
+	for($j = $i; $j <$number; $j++){
+		echo " ";
+	}
+	echo "#";
+	for($k = 0; $k < 2; $k++){
+		for($j = 1; $j < $i; $j++){
+			echo " ";
+		}
+	}
+	echo "#\n";
+}
 
