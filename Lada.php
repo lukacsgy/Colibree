d132c659e382f7efdecdb1d1b0092e467cdf6e17
György Lukács <georgelucasletters@gmail.com>
2018-02-12 11:52:22 +0100

    Kesz van.Folytatom a teteles feladatokat

--- Lada.php
+++ Lada.php
@@ -1,3 +1,32 @@
 <?php
+$boxsize = readline("Add meg a láda méretét: ");
+
+/*Teteje*/
+echo "+";
+for($i = 0; $i < $boxsize; $i++){
+	echo "-";
+}
+echo "+\n";
 
 
+for($i = 0; $i < $boxsize; $i++){
+	echo "|";
+	for ($j = 0; $j < $i; $j++){
+		echo " ";
+	}
+	echo "\\";
+	for ($j = $i + 1; $j < $boxsize; $j++){
+		echo " ";
+	}
+	echo "|\n";
+}
+
+
+
+
+/*Alja*/
+echo "+";
+for($i = 0; $i < $boxsize; $i++){
+	echo "-";
+}
+echo "+\n";
