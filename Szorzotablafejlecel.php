fd2b4ad8b92243d16ee3a6f7b54782bfa0f2424d
György Lukács <georgelucasletters@gmail.com>
2018-02-12 10:43:37 +0100

    Kesz van. Igy tudtam megoldani remÃelem szep:)

--- Szorzotablafejlecel.php
+++ Szorzotablafejlecel.php
@@ -2,22 +2,28 @@
 
 $tablesize= readline("Add meg az N szamot(max 16-ig): ");
 
-echo "  |";	
+echo "   |";	
 
 /*fejléc*/
 for($i = 1; $i <= $tablesize; $i++){
-	echo "  ".$i;
+	echo sprintf("%3s ", $i);
 }
 echo "\n";
 
 /*vezér csik*/
-echo "--+";
-for($i = 1; $i <= $tablesize; $i++){
-	echo "---";
+echo "---+";
+for($i = 1; $i < $tablesize; $i++){
+	echo ("----");
 }
+
+echo "---\n";
 
 /*szorzo tabla*/
-
-for($j = 1; $j < $tablesize; $j++ ){
-	
+for($i = 1; $i <= $tablesize; $i++){
+	echo sprintf("%3s|", $i);
+	for($j = 1; $j <= $tablesize; $j++){
+		echo sprintf("%3s ", $j * $i);
+	}
+	echo ("\n");
 }
+
