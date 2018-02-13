09e0de13c4b0dcb2dc6dd99e119029664783ffe5
György Lukács <georgelucasletters@gmail.com>
2018-02-13 10:14:42 +0100

    Javitva

--- Filehandling3.php
+++ Filehandling3.php
@@ -1,4 +1,19 @@
 <?php
 
+$fp = fopen("Text.txt", "w");
 
+if ($fp === false) {
+	echo "Error: Unable to open file\n";
+	die(1);
+}
+
+echo " The content of the file test.txt is  :\n";
+
+$txt = "test line";
+
+for ($i = 1; $i <= 4; $i++) {
+	echo $txt . $i . "\n";
+	fwrite($fp, $txt . $i . "\n");
+}
+fclose($fp);
 
