7340651ed700c128b366d62c23aea8d256b3038c
György Lukács <georgelucasletters@gmail.com>
2018-02-13 14:39:46 +0100

    Te kododat hasznaltam de while atirtam mert ugy nem azt az eredmenyt adta

--- filehandling11.php
+++ filehandling11.php
@@ -1,2 +1,25 @@
 <?php
 
+if ($argc > 2){
+	if ($argv[1] !== "--"){
+		$source = fopen($argv[1], "r");
+	}
+	else{
+		$source = STDIN;
+	}
+	
+	$target = fopen($argv[2], "w");
+	
+	do{
+		$line = fgets($source);
+		fwrite($target, $line);
+	}while($line !== FALSE);
+	
+	if (!feof){
+		echo "error";
+		die();
+	}
+	
+	fclose($source);
+	fclose($target);
+}
