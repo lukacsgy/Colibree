b4f8c9960cd015a425192d6d1431c452700be96c
György Lukács <georgelucasletters@gmail.com>
2018-02-13 16:41:49 +0100

    Kesz

--- Azosszegalegnagyobbnelkul.php
+++ Azosszegalegnagyobbnelkul.php
@@ -1,3 +1,19 @@
 <?php
 
+$size = (int) readline("Hany darab szam lesz: ");
+$first = readline("Add meg az 1. szamot: ");
+	
+$max = $first;
+$szum = $first;
 
+for($i = 1; $i < $size; $i++){
+	$current = (int) readline(" Add meg a ". ($i + 1) . " szamot: ");
+		
+	if ($current > $max){
+		$max = $current;
+	}
+	
+	$szum += $current;
+}
+
+echo "A Szamok osszege a legnagyobb nelkul : " . ($szum - $max);
