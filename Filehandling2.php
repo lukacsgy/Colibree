f0a3999f4e5db5c38e0aa5c483db537f6ca1342d
György Lukács <georgelucasletters@gmail.com>
2018-02-13 10:02:44 +0100

    Javitva

--- Filehandling2.php
+++ Filehandling2.php
@@ -2,9 +2,10 @@
 
 $fg = fopen("Text.txt", "r");
 
-if ($fp === false) {
+if ($fg === false) {
 	echo "Error: Unable to open file\n";
 	die(1);
 }
+echo "The file Text.txt created successfully...!!\n";
 echo fgets($fg);
 fclose($fg);
