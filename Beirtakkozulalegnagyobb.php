90aaba45b5d983672dcd54a30aa88329a04ca7ce
György Lukács <georgelucasletters@gmail.com>
2018-02-14 15:13:20 +0100

    Kesz de megszenvedtem a feltetelek tipusaval hogy ne booleant adja... php-n ez megekll szokni

--- Beirtakkozulalegnagyobb.php
+++ Beirtakkozulalegnagyobb.php
@@ -1,3 +1,24 @@
 <?php
 
-$firtNumber = readline("Add meg a szamokat");
+$max = 1;
+echo "Szamok legyenek nagyobbak mint 0\n";
+
+while(($current = (int)readline("Add meg a szamokat: ")) >= "0"){
+	if($current > $max){
+		$prim = false;
+		$div = 2;
+
+		while(($div < $current) && $prim != TRUE){
+			if($current % $div == 0){
+				$prim = true;
+			}
+			$div++;
+		}
+
+		if(!$prim){
+			$max = $current;
+		}
+	}
+}
+
+echo $max;
