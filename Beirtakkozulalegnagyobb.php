281065a24901d3e3ea26f3ff58f1a87a592f011c
György Lukács <georgelucasletters@gmail.com>
2018-02-14 16:24:08 +0100

    javitva Åaltalad

--- Beirtakkozulalegnagyobb.php
+++ Beirtakkozulalegnagyobb.php
@@ -1,24 +1,50 @@
 <?php
 
 $max = 1;
-echo "Szamok legyenek nagyobbak mint 0\n";
 
-while(($current = (int)readline("Add meg a szamokat: ")) >= "0"){
-	if($current > $max){
-		$prim = false;
+while (($current = (int) readline("Adjon meg egy pozitív egész számot: ")) > 0) {
+	if ($current > $max) {
+		$possiblePrim = true;
 		$div = 2;
 
-		while(($div < $current) && $prim != TRUE){
-			if($current % $div == 0){
-				$prim = true;
+		while ($possiblePrim && $div < $current) {
+			if ($current % $div == 0) {
+				$possiblePrim = false;
 			}
 			$div++;
 		}
 
-		if(!$prim){
+		if ($possiblePrim) {
 			$max = $current;
 		}
 	}
 }
 
-echo $max;
+echo "\nA Legnagyobb prim szam :" . $max;
+
+//
+//{
+//
+//	if ($current >= 0) {
+//		if ($current > $max) {
+//
+//			$prim = false;
+//			$div = 2;
+//			while (($div < $current) && $prim != true) {
+//				if ($current % $div == 0) {
+//					$prim = true;
+//				}
+//				$div++;
+//			}
+//
+//			if (!$prim) {
+//				$max = $current;
+//			}
+//		}
+//	} else {
+//		echo "Kileptel";
+//		break;
+//	}
+//}
+//
+//echo "\nA Legnagyobb prim szam :" . $max;
