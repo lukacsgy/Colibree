8477758c09cec400cd43b8b46a0ab8f7a7935991
György Lukács <georgelucasletters@gmail.com>
2018-02-15 18:22:23 +0100

    kesz

--- Azutolsootszamatlaga.php
+++ Azutolsootszamatlaga.php
@@ -1,3 +1,21 @@
 <?php
 
 
+$i = 0;
+while(($refil = readline("Add meg a tomb elemeit")) != 0){
+	
+	if ($i < 5){
+		$numbers[$i] = $refil;
+	}
+	
+	else{
+		array_shift($numbers);
+		array_push($numbers, $refil);
+		/*print_r($numbers);*/
+	}
+	$i++;
+}
+
+$numbersAvarage = array_sum($numbers)/count($numbers);
+
+echo "Az utolso 5 szam atalaga : " . $numbersAvarage;
