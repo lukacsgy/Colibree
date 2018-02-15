fffd581c1d31d5ef30c62e211919fb45291d6e20
György Lukács <georgelucasletters@gmail.com>
2018-02-15 14:48:56 +0100

    Kesz de Fseek el nem mukodott ugy....meg hasznalgatom majd

--- Mindenprimedikbetu.php
+++ Mindenprimedikbetu.php
@@ -1,4 +1,45 @@
 <?php
 
+if ($argc > 1){
+	
+	$source = fopen("Szoveg.txt", "w");
+	$text = $argv[1];
+	fwrite($source, $text);
+	
+	$source = fopen("Szoveg.txt", "r");
+	$target = fopen("Tarolo.txt", "w");
+	
+	$count = 0;
+	
+	do{
+		$char = fgetc($source);
+		$possiblePrim = false;
+		
+		if($count >= 2){
+			$possiblePrim = true;
+			$div = 2;
 
+			while ($possiblePrim && $div < $count) {
+				if ($count % $div == 0) {
+					$possiblePrim = false;
+				}
+				$div++;
+			}
+		}
+		
+		if($possiblePrim){
+			fwrite($target, $char);
+		}
+		
+		$count++;
+	}while ($char !== false);
+	
+	fclose($source);
+	fclose($target);
+	
+	unlink("Szoveg.txt");
+	rename("Tarolo.txt", "Szoveg.txt");
+	
+	
+}
 
