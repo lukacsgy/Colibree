ee052d7275ca431d429ddf060a01e19ee6f69394
György Lukács <georgelucasletters@gmail.com>
2018-02-16 09:47:38 +0100

    Adott egy m egészekből álló tömb, véletlen értékekkel feltöltve. Készíts programot, mely a felhasználótól bekért p egész számról meghatározza, hogy a tömbben p-nél nagyobb, vagy p-nél kisebb számokból van-e több

--- kissebb_nagyobb_egyenlo.php
+++ kissebb_nagyobb_egyenlo.php
@@ -1,8 +1,30 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$number1 = readline("Add meg az 'a' szamot: ");
+$number2 = readline(" Add meg a 'b' szamot: ");
+$number3 = readline(" Add meg a 'c' szamot: ");
 
+if (($number1 < $number2) && ($number1 < $number3)){
+	echo "a<";
+	if($number2 > $number3){
+		echo "c<b";
+	}
+	elseif($number3 > $number2){
+		echo "b<c";
+	}
+	else{
+		echo "b=c";
+	}
+}
+elseif (($number2 > $number1) && ($number2 > $number3)) {
+	echo "b<";
+	if($number1 > $number3){
+		echo "b>c";
+	}
+	elseif($number3 > $number2){
+		echo "c>b";
+	}
+	else{
+		echo "b=c";
+	}
+}
