0596327837ff5f4dc3109fe6d921a57fd2b60ced
György Lukács <georgelucasletters@gmail.com>
2018-02-16 09:58:05 +0100

    Kesz bar azzon gondolkodom lehete e szebbben megoldani az ifes reszt

--- Kissebekvagynagyobbak.php
+++ Kissebekvagynagyobbak.php
@@ -1,2 +1,30 @@
 <?php
 
+if($argc > 1){
+	
+	$array = [1, 1, 1, 1, 5, 7, 7, 7, 7, 1];
+	$trial = $argv[1];
+	
+	$major = 0;
+	$minor = 0;
+	foreach ($array as $value) {
+		if ($trial > $value){
+			$minor++;
+		}
+		if ($trial < $value){
+			$major++;
+		}
+	}
+	
+	if ($major > $minor){
+		echo "A megadott szamnal tobb nagyobb erteku szam van a tomben";
+	}
+	elseif ($minor > $major) {	
+		echo "A megadott szamnal tobb kisebb erteku szam van a tomben";
+	}
+	else{
+		echo "A nagyobb es kisebb erteku elemek aranya nagyabol megyezeik";
+	}
+	
+	
+}
