4c7aeb0e0d0d0be70d59fc56b52b74c6d721f145
György Lukács <georgelucasletters@gmail.com>
2018-02-16 09:45:47 +0100

    Kesz vagyok

--- Leptetes.php
+++ Leptetes.php
@@ -1,3 +1,12 @@
 <?php
 
+$array = [25, 69, 54, 8, 77, 6, 29, 10, 3, 98];
 
+print_r($array);
+
+for($i = 0; $i < 3; $i++){
+	array_push($array, $array[0]);
+	array_shift($array);
+}
+
+print_r($array);
