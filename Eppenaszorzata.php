18b2207d7819ca203979774b0b980b8724fba236
György Lukács <georgelucasletters@gmail.com>
2018-02-16 10:51:26 +0100

    Kesz

--- Eppenaszorzata.php
+++ Eppenaszorzata.php
@@ -1,2 +1,30 @@
 <?php
 
+if ($argc > 1){
+	
+	$numberZ = $argv[1];
+	
+	$array = [1, 2, 3, 4, 5];
+	
+	$possibleProduct = false;
+	foreach ($array as $value) {
+		foreach ($array as $value2) {
+			
+			$product = $value * $value2;
+			if ($product == $numberZ){
+				/* echo "Van a tomben 2 olyan szam :" . $value . " * " . $value2 . " = " . $product . "\n";*/
+				$possibleProduct = true;
+			}
+		}
+	}
+	
+	if ($possibleProduct){
+		echo "Vannak olyana szamok!";
+	}
+	else{
+		echo "Nincsen 2 olyan szam aminek szorzata a kerestt szam!";
+	}
+	
+	
+	
+}
