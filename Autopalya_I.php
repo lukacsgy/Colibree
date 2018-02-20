d07129e27d2c4a7143df09ab4677fa584602bfff
György Lukács <georgelucasletters@gmail.com>
2018-02-20 10:45:52 +0100

    Kesz van:)

--- Autopalya_I.php
+++ Autopalya_I.php
@@ -1,10 +1,27 @@
 <?php
-if ($argc > 3){
-	
-	$hour  = $argv[1];
-	$min   = $argv[2];
-	$speed = $argv[3];
-	
-	
-}
 
+	echo "Add meg az 'gyorshajtó' adatait „óra perc sebesség” formában!"
+       . "\nKiértékeléshez 0 0 0 formátumot add meg.\n";
+	
+	
+	$maxSpeed = array_fill(0, 24, 0);
+	
+	
+	do{
+	fscanf(STDIN,"%d %d %d\n" , $hour , $min ,$speed);
+	
+	if($hour == 24){
+		$hour = 0;
+	}
+	
+	if ($maxSpeed[$hour] < $speed){
+		$maxSpeed[$hour] = $speed;
+	}
+	
+	}while ($speed !== 0);
+	
+foreach ($maxSpeed as $key => $value) {
+	if($value > 0){
+		echo "\n" . sprintf("%2s",$key) . ":00-" . sprintf("%2s",$key) . ":59 -> " . sprintf("%3s",$value) . " Km/H";
+	}
+}
