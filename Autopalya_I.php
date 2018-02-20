d432d5542c95ebe1918266bdd6f873ca71b5dadd
György Lukács <georgelucasletters@gmail.com>
2018-02-20 13:07:17 +0100

    Kibovitettem a 2dik resszel. Irja ki, hogy a nap mely órájában mennyi az összes kirótt bírság! Gondolkodtam raja hogy todd diemnzios tombot erdemes lenne-e hasznalni. Visszont ara jutottam hogy 2 tomb nem uyganaz az csak hasonlo helyen tarolom azzaokt az adatokat.

--- Autopalya_I.php
+++ Autopalya_I.php
@@ -1,27 +1,42 @@
 <?php
 
-	echo "Add meg az 'gyorshajtó' adatait „óra perc sebesség” formában!"
-       . "\nKiértékeléshez 0 0 0 formátumot add meg.\n";
-	
-	
-	$maxSpeed = array_fill(0, 24, 0);
-	
-	
-	do{
-	fscanf(STDIN,"%d %d %d\n" , $hour , $min ,$speed);
-	
-	if($hour == 24){
+echo "Add meg az 'gyorshajtó' adatait „óra perc sebesség” formában!"
+ . "\nKiértékeléshez 0 0 0 formátumot add meg.\n";
+
+
+$maxSpeed = array_fill(0, 24, 0);
+$penalty = array_fill(0, 24, 0);
+
+
+do {
+	fscanf(STDIN, "%d %d %d\n", $hour, $min, $speed);
+
+	if ($hour == 24) {
 		$hour = 0;
 	}
-	
-	if ($maxSpeed[$hour] < $speed){
+
+	if ($maxSpeed[$hour] < $speed) {
 		$maxSpeed[$hour] = $speed;
 	}
-	
-	}while ($speed !== 0);
-	
-foreach ($maxSpeed as $key => $value) {
-	if($value > 0){
-		echo "\n" . sprintf("%2s",$key) . ":00-" . sprintf("%2s",$key) . ":59 -> " . sprintf("%3s",$value) . " Km/H";
+
+	if ($speed > 140) {
+		if ($speed > 180) {
+			$penalty[$hour] += 70;
+		}
+		$penalty[$hour] += 30;
+	}
+} while ($speed !== 0);
+
+
+$size = count($maxSpeed);
+
+for($i = 0; $i < $size; $i++) {
+	if ($maxSpeed[$i] > 0) {
+		echo "\n" . sprintf("%2s", $i) . ":00-" . sprintf("%2s", $i) . ":59 -> " . sprintf("%3s", $maxSpeed[$i]) . " Km/H"
+			 . "A Kisszabott buntetesek osszege az adot oraban: " . $penalty[$i] . " 000 Ft.";
 	}
 }
+
+
+
+
