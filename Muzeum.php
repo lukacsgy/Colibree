212c9599bedd818ee285db2ed3e8f5ed7afdeab6
György Lukács <georgelucasletters@gmail.com>
2018-02-21 15:38:28 +0100

    Kesz de fura ez 2 tomb dolog...

--- Muzeum.php
+++ Muzeum.php
@@ -1,3 +1,53 @@
 <?php
 
+echo"Add meg melyik nap hany latogato volt!\n"
+ . "A napokat 1-7 között add meg(ertelem szeruen hetfo-vasranapig....1-7ig) "
+ . "és utana spacel elválasztva hanyan voltak\n";
+
+
+fscanf(STDIN, "%d %d", $day[0], $visitors[0]);
+$i = 0;
+
+while ($day[$i] !== 0) {
+	$i++;
+	fscanf(STDIN, "%d %d", $day[$i], $visitors[$i]);
+	for ($j = 0; $j < $i; $j++) {
+		if ($day[$j] == $day[$i]) {
+			$visitors[$j] += $visitors[$i];
+			$visitors[$i] = 0;
+		}
+	}
+}
+array_pop($day);
+array_pop($visitors);
+
+print_r($day);
+print_r($visitors);
+
+
+
+$size = count($day);
+
+$crowdedDay = $day[0];
+$airyDay = $day[0];
+
+$maxVisit = $visitors[0];
+$minVisit = $visitors[0];
+
+for ($i = 0; $i < $size; $i++) {
+	if ($maxVisit < $visitors[$i]) {
+		$crowdedDay = $day[$i];
+		$maxVisit = $visitors[$i];
+	}
+
+	if (($minVisit > $visitors[$i]) && ($visitors[$i] != 0)) {
+		$airyDay = $day[$i];
+		$minVisit = $visitors[$i];
+	}
+}
+
+
+echo "\nA Legtobb Latogato Ezzen A Napon Van: " . $crowdedDay;
+echo "\nA Legkevesebb Latogato Ezzen A Napon Van: " . $airyDay;
+
 
