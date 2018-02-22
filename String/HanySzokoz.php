6646583119fa6d416ab00b6a2551b958ac7091d3
György Lukács <georgelucasletters@gmail.com>
2018-02-22 13:23:49 +0100

    Kesz

--- String/HanySzokoz.php
+++ String/HanySzokoz.php
@@ -1,4 +1,8 @@
 <?php
 
+$sentence = readline("Irj be egy mondatott: \n");
+
+echo "\t" . substr_count($sentence, " ") . " db szokoz van a mondatban!\n"
+ . "\tA mondat szokozok nelkul: \t" . str_replace(" ", "", $sentence);
 
 
--- String/Udvozles.php
+++ String/Udvozles.php
@@ -1,3 +1,9 @@
 <?php
 
+$name = null;
 
+for ($i = 1; $i < count($argv); $i++) {
+	$name .= $argv[$i] . " ";
+}
+
+echo "Udvozlegy " . ucwords($name);
