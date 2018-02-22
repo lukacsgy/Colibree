09bb5b520dc6c5cdd7b7e40f1b797d9b2c9b1691
György Lukács <georgelucasletters@gmail.com>
2018-02-22 12:09:00 +0100

    It's Done!

--- String/Sztringekmintkaraktertombok.php
+++ String/Sztringekmintkaraktertombok.php
@@ -1,3 +1,15 @@
 <?php
 
+$string = $argv[1];
 
+echo "A szovegben" . countOneAtStrin($string) . " db 1-es van";
+
+function countOneAtStrin($string) {
+	$lump = 0;
+	for ($i = 0; $i < strlen($string); $i++) {
+		if ($string[$i] === "1") {
+			$lump++;
+		}
+	}
+	return $lump;
+}
