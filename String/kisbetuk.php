1948122ab99913c75ee9a5c406a6e158ade4d10f
György Lukács <georgelucasletters@gmail.com>
2018-02-22 14:54:02 +0100

    Done

--- String/kisbetuk.php
+++ String/kisbetuk.php
@@ -1,1 +1,17 @@
 <?php
+
+$sentence = readline("Ird le a mondatott: ");
+
+echo "\nA Szovegben talalhato kisbetuk szama: " . countLowerCharts($sentence)
+ . "\nA szoveg csak kisbetukkel: " . strtolower($sentence);
+
+function countLowerCharts($sentence) {
+	$lowChar = 0;
+
+	for ($i = 0; $i < strlen($sentence); $i++) {
+		if (ord($sentence[$i]) >= ord("a") && ord($sentence[$i]) <= ord("z")) {
+			$lowChar++;
+		}
+	}
+	return $lowChar;
+}
