3e3ef01b4ead077d3667c025072e2eb3a200b7a0
György Lukács <georgelucasletters@gmail.com>
2018-02-22 15:25:14 +0100

    Kesz viszont ez str_replace fuggveny lenne amit Ors mutatott nekunk

--- String/Felulirascsere.php
+++ String/Felulirascsere.php
@@ -1,3 +1,21 @@
 <?php
 
+$sentence = readline("Add meg a mondatott: ");
 
+echo "Add meg hogy mit milyen karakterre csereljek és majd irjak felul!(szokozzel elvalsztva):";
+fscanf(STDIN, "%s %s", $origin, $replace);
+
+echo replaceAndOverwrite($sentence, $origin, $replace);
+
+
+function replaceAndOverwrite($sentence, $origin, $replace) {
+
+	for ($i = 0; $i < strlen($sentence); $i++) {
+		if ($sentence[$i] === $origin) {
+			$sentence[$i] = $replace;
+		} elseif ($sentence[$i] === $replace) {
+			$sentence[$i] = $origin;
+		}
+	}
+	return $sentence;
+}
