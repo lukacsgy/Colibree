12dc90c920c4af4e9694ce06f5b820e27ffbd192
György Lukács <georgelucasletters@gmail.com>
2018-02-23 15:36:53 +0100

    Kijavitottam igy mar minimum kereses is jol mukodik

--- legkisebblegnagyobb1.php
+++ legkisebblegnagyobb1.php
@@ -3,23 +3,23 @@
 $size = (int) readline("Hany darab szam lesz: ");
 
 $first = readline("Add meg az 1. szamot: ");
-	
+
 $max = $first;
 $min = $first;
 $szum = $first;
 
-for($i = 1; $i < $size; $i++){
-	$current = (int) readline(" Add meg a ". ($i + 1) . " szamot: ");
-		
-	if ($current >= $max){
+for ($i = 1; $i < $size; $i++) {
+	$current = (int) readline(" Add meg a " . ($i + 1) . " szamot: ");
+
+	if ($current >= $max) {
 		$max = $current;
 	}
-	else{
+
+	if ($min > $current) {
 		$min = $current;
 	}
-	
+
 	$szum += $current;
-	
 }
 
 echo "A Legnagyobb Szam: " . $max . "\nA Legkisebb Szam: " . $min . "\nA Szamok atlaga : " . ($szum / $size);
