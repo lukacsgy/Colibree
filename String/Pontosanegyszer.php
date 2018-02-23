e4b87dd0895c6a6539254d01cd1a686dcf53510b
György Lukács <georgelucasletters@gmail.com>
2018-02-23 11:58:55 +0100

    nem volt jo a mappaban jelszo megprobalom igy

--- String/Pontosanegyszer.php
+++ String/Pontosanegyszer.php
@@ -1,2 +1,30 @@
 <?php
 
+$textString = readline("Adj meg egy mondatott: ");
+
+$individulCharts = true;
+for ($i = 0; $i < strlen($textString); $i++) {
+	if (!charAsChar($textString, $i)) {
+		echo " $textString[$i],";
+	} else {
+		$individulCharts = false;
+	}
+}
+
+if ($individulCharts) {
+	echo "\n\tNincs Benne Ismetloddo Karakter!";
+}
+
+function charAsChar($textString, $i) {
+	$sameChart = false;
+	$j = 0;
+	while ($j < strlen($textString) && $sameChart != true) {
+		if ($j != $i) {
+			if ($textString[$i] === $textString[$j]) {
+				$sameChart = true;
+			}
+		}
+		$j++;
+	}
+	return $sameChart;
+}
--- String/Sztringekmintkaraktertombok.php
+++ String/Sztringekmintkaraktertombok.php
@@ -2,9 +2,9 @@
 
 $string = $argv[1];
 
-echo "A szovegben" . countOneAtStrin($string) . " db 1-es van";
+echo "A szovegben" . countOneAtString($string) . " db 1-es van";
 
-function countOneAtStrin($string) {
+function countOneAtString($string) {
 	$lump = 0;
 	for ($i = 0; $i < strlen($string); $i++) {
 		if ($string[$i] === "1") {
