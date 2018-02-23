3c53cf02a98e9e59c4af05eab4d68d1479c0a6bf
György Lukács <georgelucasletters@gmail.com>
2018-02-23 14:17:09 +0100

    Kesz visszont it eleg lett volna ltrim és rtrim hasznalat

--- String/Karakterektorlese.php
+++ String/Karakterektorlese.php
@@ -1,2 +1,22 @@
 <?php
 
+$parameter = readline("Add meg a szoveget: ");
+$trimChar = readline("Add meg milyen karaktert toroljunk a szoveg elejen es vegen: ");
+
+echo trimming($parameter, $trimChar);
+
+function trimming($parameter, $trimChar) {
+	$i = 0;
+	while ($parameter[$i] === $trimChar) {
+		$parameter[$i] = " ";
+		$i++;
+	}
+
+	$end = strlen($parameter) - 1;
+	while ($parameter[$end] === $trimChar) {
+		$parameter[$end] = " ";
+		$end--;
+	}
+
+	return$parameter;
+}
