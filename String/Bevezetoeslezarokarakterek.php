dc0a427fc9d22cbd6c1493a44b252fcdbfdaf944
György Lukács <georgelucasletters@gmail.com>
2018-02-23 16:49:58 +0100

    Ephogy elkezdtem

--- String/Bevezetoeslezarokarakterek.php
+++ String/Bevezetoeslezarokarakterek.php
@@ -1,2 +1,14 @@
 <?php
 
+$source = readline("Add meg a szoveget: ");
+echo "Add meg milyen karakter illeszek be és szokozzel elvaszta add meg hanyszor!";
+
+fscanf(STDIN, "%s %d", $seperator, $often);
+
+$newsolution;
+
+for($i = 0; $i < $often; $i++){
+	$newsolution[$i] = $seperator;
+}
+
+echo $newsolution;
