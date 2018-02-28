dc8c1271536d9e22256734a627050b55f9bab0bc
György Lukács <georgelucasletters@gmail.com>
2018-02-28 13:52:55 +0100

    Töltsünk fel egy nagy tömböt véletlenszámokkal. Rendezzük a tömböt. Válasszunk egy elemet a tömb értékkészletéből, és keressük meg, hogy a rendezett tömb mely tartományában szerepel (hányadiktól hányadik indexig). Tegyük ezt a keresést a lehető leggyorsabban!

---
+++ Rendezes,Rekurzio/Linearis&Binariskereses.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ Töltsünk fel egy nagy tömböt véletlenszámokkal. Rendezzük a tömböt.
+ Válasszunk egy elemet a tömb értékkészletéből, és keressük meg,
+ hogy a rendezett tömb mely tartományában szerepel (hányadiktól hányadik indexig). 
+ Tegyük ezt a keresést a lehető leggyorsabban!
+ */
+
