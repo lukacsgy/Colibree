966ed88d0faaaddeb99d65baea8ca8d107484d8b
György Lukács <georgelucasletters@gmail.com>
2018-02-28 17:24:12 +0100

    Írj függvényt, amely átvesz egy double elemekből álló tömböt, és visszaadja a medián elemét! A medián az az elem, amelynél annyi kisebb van a tömbben, ahány nagyobb. Felteheti, hogy a tömbben páratlan sok elem van, és nincs két egyforma. Tetszőlegesen használhatsz segédfüggvényeket, memóriát, de az eredeti tömböt nem változtathatod meg!

---
+++ Rendezes,Rekurzio/Median.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+Írj függvényt, amely átvesz egy double elemekből álló tömböt, és visszaadja a medián elemét! 
+A medián az az elem, amelynél annyi kisebb van a tömbben, ahány nagyobb. Felteheti, 
+hogy a tömbben páratlan sok elem van, és nincs két egyforma.
+
+Tetszőlegesen használhatsz segédfüggvényeket, memóriát, de az eredeti tömböt nem változtathatod meg!
+ */
+
