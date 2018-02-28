1fd7f71adc2bc0172a2ccdef031de9619801fa01
György Lukács <georgelucasletters@gmail.com>
2018-02-28 13:47:09 +0100

    Megoldottam visszont break-et ugy vedtem ki(Ãen is meg megoldo kulcsban is az volt....damn!) hogy olyan feltetelt adtam meghogy ne kezdje el a ciklust. Ennel szebb nem jutott eszembe.Kerdesem lehetne "szebben"?

--- Rendezes,Rekurzio/Javitottbuborekrendszer.php
+++ Rendezes,Rekurzio/Javitottbuborekrendszer.php
@@ -1,13 +1,33 @@
 <?php
 
-/* 
-A buborékrendezés egymás melletti elemeket cserél sorban. Egy sor csere hatására a legnagyobb elem a tömb végére vándorol; 
+/*
+  A buborékrendezés egymás melletti elemeket cserél sorban. Egy sor csere hatására a legnagyobb elem a tömb végére vándorol;
  * a következő körben azt már nem kell vizsgálni, hanem a tömb eggyel rövidebb részletét csak.
  *  Ezt kell folytatni addig, amíg el nem fogy a tömb.
 
-A buborékrendezés hatékonysága javítható azzal, ha megjegyezzük, hogy a vizsgált tömbrészletnél volt-e csere.
+  A buborékrendezés hatékonysága javítható azzal, ha megjegyezzük, hogy a vizsgált tömbrészletnél volt-e csere.
  *  Ha nem volt, akkor minden pár jó sorrendben van. 
  * Akkor a rövidebb részt vizsgálva is ugyanerre az eredményre jutnánk, vagyis a külső ciklust már nem kell folytatni.
  *  Implementáld ezt a változatot!
  */
 
+$tomb = [10, 6, 5, 8];
+
+
+for ($i = count($tomb) - 1; $i > 0; $i--) {
+	$csere = false;
+	for ($j = 0; $j < $i; $j++) {
+		if ($tomb[$j] > $tomb[$j + 1]) {
+			$temp = $tomb[$j];
+			$tomb[$j] = $tomb[$j + 1];
+			$tomb[$j + 1] = $temp;
+			$csere = true;
+		}
+	}
+	
+	if (!$csere) {
+		$i = 0;
+	}
+	
+	print_r($tomb);
+}
