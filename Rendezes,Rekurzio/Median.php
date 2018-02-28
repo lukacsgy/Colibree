b51e77a08479fad3374242b7a251dd9fd103da4f
György Lukács <georgelucasletters@gmail.com>
2018-02-28 18:01:54 +0100

    Idaig juttoam vele eddig

--- Rendezes,Rekurzio/Median.php
+++ Rendezes,Rekurzio/Median.php
@@ -1,10 +1,19 @@
 <?php
 
-/* 
-Írj függvényt, amely átvesz egy double elemekből álló tömböt, és visszaadja a medián elemét! 
-A medián az az elem, amelynél annyi kisebb van a tömbben, ahány nagyobb. Felteheti, 
-hogy a tömbben páratlan sok elem van, és nincs két egyforma.
+/*
+  Írj függvényt, amely átvesz egy double elemekből álló tömböt, és visszaadja a medián elemét!
+  A medián az az elem, amelynél annyi kisebb van a tömbben, ahány nagyobb. Felteheti,
+  hogy a tömbben páratlan sok elem van, és nincs két egyforma.
 
-Tetszőlegesen használhatsz segédfüggvényeket, memóriát, de az eredeti tömböt nem változtathatod meg!
+  Tetszőlegesen használhatsz segédfüggvényeket, memóriát, de az eredeti tömböt nem változtathatod meg!
  */
 
+$array = [];
+$array = randFloatArrayGenerator($array, $arraySize);
+
+function randFloatArrayGenerator($array, $arraySize) {
+	for ($i = 0; $i < $arraySize; $i++) {
+		$array[$i] = rand() / mt_getrandmax();
+	}
+	return $array;
+}
