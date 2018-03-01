1acc1135c25fbf14eb5872e7dae41440ebb58964
György Lukács <georgelucasletters@gmail.com>
2018-03-01 18:06:03 +0100

    Eddig jutottam ma

--- Rendezes,Rekurzio/Zarojelekparjai.php
+++ Rendezes,Rekurzio/Zarojelekparjai.php
@@ -5,3 +5,17 @@
 (A zárójelek (egymásba () lehetnek)) ágyazva.Írd meg ugyanezt iteratívan is!
  */
 
+$string = readline("Add meg a szovget: ");
+
+for ($i = 0; $i < strlen($string); $i++){
+	
+}
+
+function findTheBracket($string, $i = 0){
+	
+	if($string[$i] != ")"){
+		return findTheBracket($string, $i + 1);	
+	}
+	
+	return $i;
+}
