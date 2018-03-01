68dcfaf5b8707126d9227f4018b8f56e7770ddc7
György Lukács <georgelucasletters@gmail.com>
2018-03-01 16:04:14 +0100

    Kesz de a Dani segitett mert rosszul kodoltam le

--- Rendezes,Rekurzio/Bemenetmegforditas.php
+++ Rendezes,Rekurzio/Bemenetmegforditas.php
@@ -1,8 +1,21 @@
 <?php
 
-/* 
-Írj rekurziót használó programot, amely beolvassa a szabványos bemenetén érkező karaktersorozatot, 
-és visszafelé írja ki azt! A program ne használjon tömböket!*/
+/*
+  Írj rekurziót használó programot, amely beolvassa a szabványos bemenetén érkező karaktersorozatot,
+  és visszafelé írja ki azt! A program ne használjon tömböket! */
+
+$string = readline("Add meg a szoveget: ");
+echo contraiwise($string, strlen($string));
 
 
+function contraiwise($string, $stringLength) {
 
+	if ($stringLength > 0) {
+		echo $string[$stringLength - 1];
+		return contraiwise($string, $stringLength - 1);
+	}
+}
+
+//for ($i = strlen($string) - 1; $i >= 0; $i--){
+//	echo $string[$i];
+//
