c2cfc3d6297292f605569fc63240c8421f40af32
György Lukács <georgelucasletters@gmail.com>
2018-03-01 14:34:46 +0100

    Írj rekurziót használó programot, amely beolvassa a szabványos bemenetén érkező karaktersorozatot, és visszafelé írja ki azt! A program ne használjon tömböket!

---
+++ Rendezes,Rekurzio/Bemenetmegforditas.php
@@ -0,0 +1,8 @@
+<?php
+
+/* 
+Írj rekurziót használó programot, amely beolvassa a szabványos bemenetén érkező karaktersorozatot, 
+és visszafelé írja ki azt! A program ne használjon tömböket!*/
+
+
+
