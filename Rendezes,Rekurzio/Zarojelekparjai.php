17918a84a4965c35b22c726ef3edc5b498a252ee
György Lukács <georgelucasletters@gmail.com>
2018-03-01 17:38:16 +0100

    Írj rekurzív programot, amelyik egy nyitó zárójellel kezdődő sztringben megtalálja a zárójel bezáró párját. (A zárójelek (egymásba () lehetnek)) ágyazva.Írd meg ugyanezt iteratívan is!

---
+++ Rendezes,Rekurzio/Zarojelekparjai.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+Írj rekurzív programot, amelyik egy nyitó zárójellel kezdődő sztringben megtalálja a zárójel bezáró párját.
+(A zárójelek (egymásba () lehetnek)) ágyazva.Írd meg ugyanezt iteratívan is!
+ */
+
