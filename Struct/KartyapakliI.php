d3b02c52b1c6ac29fc6c4abf6e0dc7f3dd9abaed
György Lukács <georgelucasletters@gmail.com>
2018-03-02 13:06:31 +0100

    Kártyás játékot írunk. Definiálj egy olyan C-s típust, amely tárolhatja egy kártya adatait (szín: pikk, treff, … és szám: A, 2, 3, … J, Q, K). Tölts fel egy tömböt egy pakli kártyáival. Utána keverd meg a tömböt. A keverő algoritmus ne cserélje feleslegesen sokszor a tömb elemeit! Végül írd ki, milyen sorrendben szerepelnek a kártyák a megkevert pakliban.

---
+++ Struct/KartyapakliI.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+Kártyás játékot írunk. Definiálj egy olyan C-s típust, 
+ amely tárolhatja egy kártya adatait (szín: pikk, treff, … és szám: A, 2, 3, … J, Q, K). 
+ Tölts fel egy tömböt egy pakli kártyáival. Utána keverd meg a tömböt. 
+ A keverő algoritmus ne cserélje feleslegesen sokszor a tömb elemeit! 
+ Végül írd ki, milyen sorrendben szerepelnek a kártyák a megkevert pakliban.
+ */
+
