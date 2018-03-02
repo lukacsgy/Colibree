1b5a16ddbb846754ff682be4145c56d1d99ed247
György Lukács <georgelucasletters@gmail.com>
2018-03-02 10:17:30 +0100

    Egy tömb rendezve: a legelejére rakjuk a legkisebb elemet, utána rendezzük a tömb fennmaradó részét. Minden iteráció átírható rekurzióvá: írjuk meg a közvetlen kiválasztásos rendezőt rekurzívan!

---
+++ Rendezes,Rekurzio/Rendezeskozvetlenkivalsztassal.php
@@ -0,0 +1,7 @@
+<?php
+
+/* 
+Egy tömb rendezve: a legelejére rakjuk a legkisebb elemet, utána rendezzük a tömb fennmaradó részét. 
+Minden iteráció átírható rekurzióvá: írjuk meg a közvetlen kiválasztásos rendezőt rekurzívan!
+ */
+
