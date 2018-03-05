6e051b8963ec6ff80920958858828ce2047c4f31
György Lukács <georgelucasletters@gmail.com>
2018-03-05 11:13:48 +0100

    Készíts parancsot ami berögzít egy szállítást!

---
+++ Struct/shipments.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+Szállítás tervező program
+
+Írjatok programot, amivel szállításokat lehet tervezni. Egy szállítás két fél között jön létre.
+Szállítások tervezéskor az alábbi dolgok érdekelnek minket:
+
+- a szállítás végpontjai (a két fél nevei)
+- a kamion tervezett érkezése a végpontokhoz
+- a szállító jármű rendszáma
+- a tervezett felrakodás ideje percben 
+- a tervezett lerakodás ideje percben
+
+Készíts parancsot ami berögzít egy szállítást!
+Készíts parancsot ami listázza a rögzített szállításokat!
+ */
+
