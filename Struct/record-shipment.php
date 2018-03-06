1e3d6a222236efdfe29348d9e3048f11c0e5b5bd
György Lukács <georgelucasletters@gmail.com>
2018-03-06 14:57:11 +0100

    Alap feledat es a feature request #1 kesz

--- Struct/record-shipment.php
+++ Struct/record-shipment.php
@@ -2,53 +2,65 @@
 
 $packName = readline("Add Meg A Felrakodas Helyet(Ceg nev):\t ");
 $packDate = readline("Add meg Felpakolas Idopontjat(Év Honap Nap Ora:Perc) Szam Formatumban:\t");
+$packDuration = readline("Hany Percig Tart Majd A Felpakolas(percben):\t ");
 
-if (!emptyPlace("shipment.txt", $packName, $packDate)) {
-	echo "Az Idopont foglalt. Kerjuk Adjon Meg Új Idopontott";
-} else {
+emptyPlace("shipment.txt", $packDate, $packDuration);
 
-	$packDuration = readline("Hany Percig Tart Majd A Felpakolas(percben):\t ");
+$lodgName = readline("Add Meg A Lerakodas Helyet(Ceg nev): \t");
+$lodgDate = readline("Add meg Lerakodas Idopontjat(Év-Honap-Nap Ora:Perc) Szam Formatumban:\t");
+$lodgDuration = readline("Hany Percig Tart Majd A Lerakodas(percben):\t ");
 
-	$lodgName = readline("Add Meg A Lerakodas Helyet(Ceg nev): \t");
-	$lodgDate = readline("Add meg Lerakodas Idopontjat(Év-Honap-Nap Ora:Perc) Szam Formatumban:\t");
+emptyPlace("shipment.txt", $lodgDate, $lodgDuration);
 
-	if (!emptyPlace("shipment.txt", $lodgName, $lodgDate)) {
-		echo "Az Idopont foglalt. Kerjuk Adjon Meg Új Idopontott";
-	} else {
+$licensePlateNumber = readline("Add Meg A Kamion Rendszamat: \t");
 
-		$lodgDuration = readline("Hany Percig Tart Majd A Lerakodas(percben):\t ");
+$fp = fopen("shipment.txt", "a+");
 
-		$licensePlateNumber = readline("Add Meg A Kamion Rendszamat: \t");
+file_put_contents("shipment.txt", $packName . "\n", FILE_APPEND);
+file_put_contents("shipment.txt", $packDate . "\n", FILE_APPEND);
+file_put_contents("shipment.txt", $packDuration . "\n", FILE_APPEND);
 
-		$fp = fopen("shipment.txt", "a+");
+file_put_contents("shipment.txt", $lodgName . "\n", FILE_APPEND);
+file_put_contents("shipment.txt", $lodgDate . "\n", FILE_APPEND);
+file_put_contents("shipment.txt", $lodgDuration . "\n", FILE_APPEND);
 
-		file_put_contents("shipment.txt", $packName . "\n", FILE_APPEND);
-		file_put_contents("shipment.txt", $packDate . "\n", FILE_APPEND);
-		file_put_contents("shipment.txt", $packDuration . "\n", FILE_APPEND);
+file_put_contents("shipment.txt", $licensePlateNumber . "\n", FILE_APPEND);
 
-		file_put_contents("shipment.txt", $lodgName . "\n", FILE_APPEND);
-		file_put_contents("shipment.txt", $lodgDate . "\n", FILE_APPEND);
-		file_put_contents("shipment.txt", $lodgDuration . "\n", FILE_APPEND);
+fclose($fp);
 
-		file_put_contents("shipment.txt", $licensePlateNumber . "\n", FILE_APPEND);
 
-		fclose($fp);
-	}
-}
 
-function emptyPlace($file, $placeName, $packDate) {
+
+function emptyPlace($file, $packDate, $packDuration) {
 	$checkingArray = file("shipment.txt");
 
 	for ($i = 0; $i < count($checkingArray); $i++) {
-		if (trim($checkingArray[$i]) === $placeName) {
-				if(trim($checkingArray[$i + 1]) === $packDate) {
-				return false;
+		$date = $checkingArray[$i];
+
+		if (dateConverter($date) === dateConverter($packDate)) {
+			$duration = $checkingArray[$i + 1];
+
+			if (startTime($packDate) >= startTime($date) ||
+					endTime($packDate, $packDuration) >= startTime($date) ||
+					(startTime($packDate) < startTime($date)) &&
+					(endTime($packDate, $packDuration) >= startTime($date))) {
+				
+				echo "\n\t\tAz napra mar van pakolas! $date";
 			}
 		}
 	}
+
 	return true;
 }
 
-function filehandling($array){
-	
+function dateConverter($date) {
+	return date("Y-m-d", strtotime($date));
+}
+
+function startTime($date) {
+	return date("H:i", strtotime($date));
+}
+
+function endTime($date, $duration) {
+	return date("H:i", strtotime($date) + (intval($duration) * 60));
 }
