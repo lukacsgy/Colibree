e3ff877d1886806c55e2733fe0d5c839f0ea486f
György Lukács <georgelucasletters@gmail.com>
2018-03-06 15:00:19 +0100

    Lehessen listázni azokat szállításokat, amiknek az egyik végpontja egy felhasználó által nevesített végpont.

---
+++ Struct/list-shipments-located.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+Lehessen listázni azokat szállításokat, 
+amiknek az egyik végpontja egy felhasználó által nevesített végpont.
+A lista csak a végponton szükséges információkat tartalmazza. 
+Továbbiak a csatolt tesztben.
+ */
+
