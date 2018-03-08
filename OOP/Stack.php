bb744122c01c6237cc6c6034e28cd20434d08a40
György Lukács <georgelucasletters@gmail.com>
2018-03-08 14:14:19 +0100

    Kesz

--- OOP/Stack.php
+++ OOP/Stack.php
@@ -1,6 +1,30 @@
 <?php
 
-/* 
-#1 Valósítsd meg a Stack (verem) adatszerkezetet objektum orientáltan, osztályként!
-*/
+/*
+  #1 Valósítsd meg a Stack (verem) adatszerkezetet objektum orientáltan, osztályként!
+ */
+
+class Stack {
+
+	private $array = [];
+
+	public function push($data) {
+		$this->array[] = $data;
+	}
+
+	public function pop() {
+		return array_pop($this->array);
+	}
+
+}
+
+$teszt = new Stack();
+
+$teszt->push(10);
+$teszt->push(20);
+$teszt->push(30);
+
+echo $teszt->pop();
+echo $teszt->pop();
+echo $teszt->pop();
 
