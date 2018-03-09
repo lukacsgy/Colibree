71ccf8900b583baba6b37dfae875cf5a75721d41
György Lukács <georgelucasletters@gmail.com>
2018-03-09 15:36:54 +0100

    Racionalis muvleetek oop-n

---
+++ OOP/Decimal.php
@@ -0,0 +1,48 @@
+<?php
+
+class Rational {
+	
+	/**
+     * @var int
+     */
+	private $denominator;
+	
+	/**
+     * @var int
+     */
+	private $numerator;
+	
+	public function __construct($denominator, $numerator) {
+		
+	}
+	
+	public function add(Rational $rational) {
+		
+	}
+	
+	public function sub(Rational $rational) {
+		
+	}
+	
+	public function multiplying(Rational $rational) {
+		
+	}
+	
+	public function division(Rational $rational) {
+		
+	}
+	
+	public function toFloat() {
+		
+	}
+	
+	public function toStirng() {
+		
+	}
+	
+	public function isEquals(Rational $rational){
+		
+	}
+	
+}
+
